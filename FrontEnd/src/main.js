e866b9f33eecfe22fc5cb1eb90b1bf037ec0ea83
Pazim Goyal <pazimgoyal@gmail.com>
2025-06-22 11:46:40 -0400

    reshuffling
    god knows why i did this

---
+++ FrontEnd/src/main.js
@@ -0,0 +1,179 @@
+"use strict";
+var lat = 43.70473;
+var long = -79.755893;
+document.addEventListener("DOMContentLoaded", () => {
+    // Map initialisation
+    var map = L.map("map").setView([lat, long], 15);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+    var layerGroup = L.layerGroup().addTo(map);
+    L.marker([lat, long]).addTo(layerGroup);
+    updateForm(lat, long);
+    fetch_address(lat, long);
+    plotlines(lat, long, 180);
+    map.on("click", popup);
+    const search_form = document.getElementById("searchForm");
+    const lat_long_form = document.getElementById("latLongForm");
+    const latitude = document.getElementById("latitude");
+    const longitude = document.getElementById("longitude");
+    const offset = document.getElementById("offset");
+    const locate_marker = document.getElementById("locate_marker");
+    // functions
+    function loadingBar() {
+        const div = document.getElementById("loading_overlay");
+        if (div.style.display === "flex" || !div.style.display) {
+            div.style.display = "none"; // or "flex", "grid", etc.
+        }
+        else {
+            div.style.display = "flex";
+        }
+    }
+    function updateForm(latitude, longitude) {
+        const latInput = document.getElementById("latitude");
+        const lonInput = document.getElementById("longitude");
+        if (latInput && lonInput) {
+            latInput.value = latitude.toString();
+            lonInput.value = longitude.toString();
+        }
+    }
+    function popup(e) {
+        layerGroup.clearLayers();
+        L.marker(e.latlng).addTo(layerGroup);
+        console.log(e.latlng);
+        updateForm(e.latlng.lat, e.latlng.lng);
+        fetch_address(e.latlng.lat, e.latlng.lng);
+    }
+    function update_address(address) {
+        const disp_address = document.getElementById("disp_address");
+        disp_address.innerHTML = address;
+    }
+    function fetch_address(lat, long) {
+        fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json`, {
+            headers: {},
+        })
+            .then((res) => res.json())
+            .then((res) => {
+            update_address(res.display_name);
+        });
+    }
+    function fetch_latlong(address) {
+        console.log(address);
+        fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address.toString())}`)
+            .then((res) => res.json())
+            .then((data) => {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            updateForm(lat, lon);
+            update_address(address.toString());
+        });
+    }
+    search_form.addEventListener("submit", (event) => {
+        loadingBar();
+        event.preventDefault();
+        const address = document.getElementById("search_address");
+        fetch_latlong(address.toString());
+        loadingBar();
+    });
+    lat_long_form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const latitude_val = latitude.value.trim();
+        const longitude_val = longitude.value.trim();
+        const offset_val = offset.value.trim();
+        if (!latitude_val || !longitude_val)
+            return;
+        loadingBar();
+        try {
+            const response = fetch("http://127.0.0.1:8000/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    longitude: longitude_val,
+                    latitude: latitude_val,
+                    offset: offset_val,
+                }),
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                const output = document.getElementById("output");
+                output.innerHTML = "";
+                for (var key in res) {
+                    // if (key == "timestamp" || key == "GHI") {
+                    if (key.startsWith("tab_")) {
+                        continue;
+                    }
+                    let inner = `<div class="grid_obj"><h3>${key}</h3><h4>${res[key]}<h4></div>`;
+                    output.innerHTML = output.innerHTML + inner;
+                }
+                const ghi = res["tab_GHI"];
+                const dhi = res["tab_DHI"];
+                const dni = res["tab_DNI"];
+                const time = res["tab_timestamp"];
+                const list = document.getElementById("solar_in");
+                if (list) {
+                    let inner = "<h2>Solar insolation intensity estimates</h2><table><tr><th>DATE TIME</th><th>GHI</th><th>DHI</th><th>DNI</th><tr>";
+                    for (let i = 0; i < time.length; i++) {
+                        inner += `<tr><td>${time[i]}</td><td>${ghi[i]} W/m²</td><td>${dhi[i]} W/m²</td><td>${dni[i]} W/m²</td></tr>`;
+                    }
+                    inner += `</table>`;
+                    list.innerHTML = inner;
+                }
+                if ("optimal Azimuth" in res) {
+                    plotlines(Number(latitude_val), Number(longitude_val), res["optimal Azimuth"]);
+                }
+                loadingBar();
+            });
+        }
+        catch (error) {
+            loadingBar();
+            console.error("POST request failed:", error);
+        }
+    });
+    locate_marker.addEventListener("click", () => {
+        const latitude_val = Number(latitude.value.trim());
+        const longitude_val = Number(longitude.value.trim());
+        let obj = { latlng: { lat: latitude_val, lng: longitude_val } };
+        popup(obj);
+        const latlng = new L.LatLng(latitude_val, longitude_val);
+        map.panTo(latlng);
+    });
+    function plotlines(latitude, longitude, azimuth) {
+        const length = 0.001;
+        const azimuthRad = (azimuth * Math.PI) / 180;
+        const endLat = latitude + length * Math.cos(azimuthRad);
+        const endLng = longitude + length * Math.sin(azimuthRad);
+        const line = L.polyline([
+            [latitude, longitude],
+            [endLat, endLng],
+        ], {
+            color: "red",
+            weight: 3,
+        }).addTo(layerGroup);
+        // Optional: arrow icon (simulate direction)
+        // L.circleMarker([endLat, endLng], {
+        //   radius: 4,
+        //   color: "red",
+        //   fillOpacity: 3,
+        // }).addTo(layerGroup);
+        const arrowSize = 0.0003;
+        // const arrowTip = [endLat, endLng];
+        const leftWingc = [
+            endLat - arrowSize * Math.cos(azimuthRad - Math.PI / 6),
+            endLng - arrowSize * Math.sin(azimuthRad - Math.PI / 6),
+        ];
+        const rightWingc = [
+            endLat - arrowSize * Math.cos(azimuthRad + Math.PI / 6),
+            endLng - arrowSize * Math.sin(azimuthRad + Math.PI / 6),
+        ];
+        const arrowTip = new L.LatLng(endLat, endLng);
+        const leftWing = new L.LatLng(leftWingc[0], leftWingc[1]);
+        const rightWing = new L.LatLng(rightWingc[0], rightWingc[1]);
+        L.polygon([arrowTip, leftWing, rightWing], {
+            color: "red",
+            fillColor: "red",
+            fillOpacity: 1,
+        }).addTo(layerGroup);
+    }
+});
---
+++ FrontEnd/src/main.ts
@@ -0,0 +1,234 @@
+var lat = 43.70473;
+var long = -79.755893;
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Map initialisation
+  var map = L.map("map").setView([lat, long], 15);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  var layerGroup = L.layerGroup().addTo(map);
+  L.marker([lat, long]).addTo(layerGroup);
+
+  updateForm(lat, long);
+  fetch_address(lat, long);
+  plotlines(lat, long, 180);
+  map.on("click", popup);
+
+  const search_form = document.getElementById("searchForm") as HTMLFormElement;
+
+  const lat_long_form = document.getElementById(
+    "latLongForm"
+  ) as HTMLFormElement;
+
+  const latitude = document.getElementById("latitude") as HTMLInputElement;
+  const longitude = document.getElementById("longitude") as HTMLInputElement;
+  const offset = document.getElementById("offset") as HTMLInputElement;
+
+  const locate_marker = document.getElementById(
+    "locate_marker"
+  ) as HTMLButtonElement;
+
+  // functions
+  function loadingBar(): void {
+    const div = document.getElementById("loading_overlay") as HTMLInputElement;
+    if (div.style.display === "flex" || !div.style.display) {
+      div.style.display = "none"; // or "flex", "grid", etc.
+    } else {
+      div.style.display = "flex";
+    }
+  }
+
+  function updateForm(latitude: number, longitude: number) {
+    const latInput = document.getElementById("latitude") as HTMLInputElement;
+    const lonInput = document.getElementById("longitude") as HTMLInputElement;
+
+    if (latInput && lonInput) {
+      latInput.value = latitude.toString();
+      lonInput.value = longitude.toString();
+    }
+  }
+
+  function popup(e: any) {
+    layerGroup.clearLayers();
+    L.marker(e.latlng).addTo(layerGroup);
+    console.log(e.latlng);
+    updateForm(e.latlng.lat, e.latlng.lng);
+    fetch_address(e.latlng.lat, e.latlng.lng);
+  }
+
+  function update_address(address: string) {
+    const disp_address = document.getElementById(
+      "disp_address"
+    ) as HTMLInputElement;
+    disp_address.innerHTML = address;
+  }
+
+  function fetch_address(lat: number, long: number) {
+    fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json`,
+      {
+        headers: {},
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        update_address(res.display_name);
+      });
+  }
+
+  function fetch_latlong(address: string) {
+    console.log(address);
+    fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+        address.toString()
+      )}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+
+        updateForm(lat, lon);
+        update_address(address.toString());
+      });
+  }
+
+  search_form.addEventListener("submit", (event: SubmitEvent) => {
+    loadingBar();
+    event.preventDefault();
+
+    const address = document.getElementById(
+      "search_address"
+    ) as HTMLInputElement;
+    fetch_latlong(address.toString());
+    loadingBar();
+  });
+
+  lat_long_form.addEventListener("submit", (event: SubmitEvent) => {
+    event.preventDefault();
+
+    const latitude_val = latitude.value.trim();
+    const longitude_val = longitude.value.trim();
+    const offset_val = offset.value.trim();
+
+    if (!latitude_val || !longitude_val) return;
+
+    loadingBar();
+    try {
+      const response = fetch("http://127.0.0.1:8000/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          longitude: longitude_val,
+          latitude: latitude_val,
+          offset: offset_val,
+        }),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          const output: HTMLDivElement = document.getElementById(
+            "output"
+          ) as HTMLDivElement;
+          output.innerHTML = "";
+
+          for (var key in res) {
+            // if (key == "timestamp" || key == "GHI") {
+            if (key.startsWith("tab_")) {
+              continue;
+            }
+            let inner = `<div class="grid_obj"><h3>${key}</h3><h4>${res[key]}<h4></div>`;
+            output.innerHTML = output.innerHTML + inner;
+          }
+
+          const ghi = res["tab_GHI"];
+          const dhi = res["tab_DHI"];
+          const dni = res["tab_DNI"];
+          const time = res["tab_timestamp"];
+          const list = document.getElementById("solar_in");
+
+          if (list) {
+            let inner =
+              "<h2>Solar insolation intensity estimates</h2><table><tr><th>DATE TIME</th><th>GHI</th><th>DHI</th><th>DNI</th><tr>";
+
+            for (let i = 0; i < time.length; i++) {
+              inner += `<tr><td>${time[i]}</td><td>${ghi[i]} W/m²</td><td>${dhi[i]} W/m²</td><td>${dni[i]} W/m²</td></tr>`;
+            }
+            inner += `</table>`;
+            list.innerHTML = inner;
+          }
+
+          if ("optimal Azimuth" in res) {
+            plotlines(
+              Number(latitude_val),
+              Number(longitude_val),
+              res["optimal Azimuth"]
+            );
+          }
+
+          loadingBar();
+        });
+    } catch (error) {
+      loadingBar();
+      console.error("POST request failed:", error);
+    }
+  });
+
+  locate_marker.addEventListener("click", () => {
+    const latitude_val = Number(latitude.value.trim());
+    const longitude_val = Number(longitude.value.trim());
+    let obj = { latlng: { lat: latitude_val, lng: longitude_val } };
+    popup(obj);
+    const latlng = new L.LatLng(latitude_val, longitude_val);
+    map.panTo(latlng);
+  });
+
+  function plotlines(latitude: number, longitude: number, azimuth: number) {
+    const length = 0.001;
+    const azimuthRad = (azimuth * Math.PI) / 180;
+    const endLat = latitude + length * Math.cos(azimuthRad);
+    const endLng = longitude + length * Math.sin(azimuthRad);
+    const line = L.polyline(
+      [
+        [latitude, longitude],
+        [endLat, endLng],
+      ],
+      {
+        color: "red",
+        weight: 3,
+      }
+    ).addTo(layerGroup);
+
+    // Optional: arrow icon (simulate direction)
+    // L.circleMarker([endLat, endLng], {
+    //   radius: 4,
+    //   color: "red",
+    //   fillOpacity: 3,
+    // }).addTo(layerGroup);
+
+    const arrowSize = 0.0003;
+    // const arrowTip = [endLat, endLng];
+    const leftWingc = [
+      endLat - arrowSize * Math.cos(azimuthRad - Math.PI / 6),
+      endLng - arrowSize * Math.sin(azimuthRad - Math.PI / 6),
+    ];
+    const rightWingc = [
+      endLat - arrowSize * Math.cos(azimuthRad + Math.PI / 6),
+      endLng - arrowSize * Math.sin(azimuthRad + Math.PI / 6),
+    ];
+    const arrowTip = new L.LatLng(endLat, endLng);
+    const leftWing = new L.LatLng(leftWingc[0], leftWingc[1]);
+    const rightWing = new L.LatLng(rightWingc[0], rightWingc[1]);
+
+    L.polygon([arrowTip, leftWing, rightWing], {
+      color: "red",
+      fillColor: "red",
+      fillOpacity: 1,
+    }).addTo(layerGroup);
+  }
+});
